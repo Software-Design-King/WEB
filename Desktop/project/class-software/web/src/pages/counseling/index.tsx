0ac3c311f47d4ab99b9033bc4c4ee949d28892cf
Kwon Dohun <154305826+Kwon2do@users.noreply.github.com>
2025-03-20 17:13:43 +0900

    Delete Desktop/project directory

--- Desktop/project/class-software/web/src/pages/counseling/index.tsx
+++
@@ -1,9 +0,0 @@
-//프로젝트 세팅
-
-import React from "react";
-
-const CounselingPage = () => {
-  return <>상담내역 관리페이지</>;
-};
-
-export default CounselingPage;
--- Desktop/project/class-software/web/src/pages/login/index.tsx
+++
@@ -1,9 +0,0 @@
-//프로젝트 세팅
-
-import React from "react";
-
-const LoginPage = () => {
-  return <>로그인페이지</>;
-};
-
-export default LoginPage;
